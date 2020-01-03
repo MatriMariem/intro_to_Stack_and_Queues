31cca0ee97fc9cc1b432370f6aa967b73c8e0e76
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-01-03 11:15:58 -0800

    ops

--- treat_line.c
+++ treat_line.c
@@ -5,7 +5,9 @@ void treat_line(stack_t **head, char *line, unsigned int line_number)
         char *token = NULL;
 
         token = strtok(line , " \n");
-        while (token == " ")
-                token = strtok(null, " \n");
+        while (token[0] == ' ')
+                token = strtok(NULL, " \n");
+	if (!token)
+		return;
         treat_token(token);
 }
--- treat_token.c
+++ treat_token.c
@@ -1,6 +1,43 @@
 #include "header.h"
 
-void treat_line(char *token)
+void treat_token(stack_t **head, char *token)
 {
+	char *arg;
+	instruction_t *ops;
+
+	if (!strncmp("push", token, 4))
+	{
+		if ((arg = check_push_arg(token)) != NULL)
+			push(head, line_number, arg);
+	}
 	
+	ops = create_ops();
+
+	/* cas 2 if opcode*/
+	/* cas 3 if invalid*/
+}
+
+instruction_t *create_ops(void)
+{
+	instruction_t ops[] = {
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"add", _add},
+		{"nop", _nop},
+		{"sub", _sub},
+		{"div", _div},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{"rotl", _rotl},
+		{"rotr", _rotr},
+		{"stack", _stack},
+		{"queue", _queue},
+		{NULL, NULL}
+	};
+
+	return (ops);
 }
