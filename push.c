3e4aef25ee48e823d7680d037938dfb09450cd95
edin93 <il_matador@hotmail.fr>
2020-01-04 00:26:14 +0000

    debugging error in check_push_arg function

--- push.c
+++ push.c
@@ -46,9 +46,11 @@ char *check_push_arg(char *token)
 
 	if (!token2)
 		get_usage_err();
-
-	for (len = 0; (token2[len] != '\n' && token2[len] != ' '); len++)
+	printf("BEFORE LOOP\n");
+	for (len = 0; (token2[len] && token2[len] != '\n' && token2[len] != ' '); len++)
 		;
+	printf("AFTER LOOP\n");
+
 	arg = malloc(sizeof(char) * (len + 1));
 	if (!arg)
 		get_usage_err();
